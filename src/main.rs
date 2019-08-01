9cc7f70c2545b63fc20c4f3b209f3b4c26c779a8
tanukkicchi <falguialexander7@gmail.com>
2019-08-01 15:45:30 +0800

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    const FILE_LOC_MENU_FIRST_MESSAGE: &str = "./menu/first";
+
+    display_menu_message(FILE_LOC_MENU_FIRST_MESSAGE);
+
+    //let mut choice;
+
+
+}
+
+fn display_menu_message(floc: &str) {
+
+}
